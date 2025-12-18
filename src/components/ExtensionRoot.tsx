0de56fe79cdb72575732f3fcb88f0b27bea87b5a
Kyle d'Oliveira <kyle@aha.io>
2025-12-18 14:50:05 -0800

    Start getting the code working for github enterprise

--- src/components/ExtensionRoot.tsx
+++ src/components/ExtensionRoot.tsx
@@ -1,15 +1,33 @@
 import { AuthProvider } from "@aha-app/aha-develop-react";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Styles } from "./Styles";
+import { IDENTIFIER } from "extension";
 
 /**
  * Set up the styles and auth provider
  */
 export const ExtensionRoot: React.FC<{}> = ({ children }) => {
+  const [serviceName, setServiceName] = useState(null as null | string);
+
+  useEffect(() => {
+    async function determineService() {
+      const clientId = await aha.settings.get(`${IDENTIFIER}.oauthClientId`);
+
+      if (clientId) {
+        setServiceName("github_enterprise");
+      } else {
+        setServiceName("github");
+      }
+    }
+    determineService();
+  }, []);
+
+  if (!serviceName) return null;
+
   return (
     <>
       <Styles />
-      <AuthProvider serviceName="github" serviceParameters={{ scope: "repo" }}>
+      <AuthProvider serviceName={serviceName} serviceParameters={{ scope: "repo" }}>
         {children}
       </AuthProvider>
     </>
